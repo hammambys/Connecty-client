4681304aa9e803e1a614e21ad53a7a9cd2aa7d14
unknown <hammambenyounes@gmail.com>
2021-10-10 12:45:14 +0100

    .

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://stark-mesa-29168.herokuapp.com/", //http://localhost:5000/
+  uri: "https://cors-anywhere.herokuapp.com/https://connecty-server.herokuapp.com/", //http://localhost:5000/
 });
 
 const authLink = setContext(() => {
