0bee722aa747818946240886dd9382ea2fd6a879
unknown <hammambenyounes@gmail.com>
2021-10-10 12:55:23 +0100

    .

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,11 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-<<<<<<< HEAD
-  uri: "https://cors-anywhere.herokuapp.com/https://connecty-server.herokuapp.com/", //http://localhost:5000/
-=======
-  uri: "https://connecty-server.herokuapp.com/",  //https://connecty-server.herokuapp.com/
->>>>>>> b7ccd20646899044da309ec0fadbb9dda932a4fb
+  uri: "https://cors-anywhere.herokuapp.com/https://connecty-back.herokuapp.com/", //http://localhost:5000/
 });
 
 const authLink = setContext(() => {
